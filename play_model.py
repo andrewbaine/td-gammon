44ddfe0c1bb3974192e14f7fd4ddce4eb69f9a08
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-03-25 11:30:13 -0400

    easier testing of models

--- play_model.py
+++ play_model.py
@@ -1,30 +1,37 @@
+import argparse
+
 import torch
+
 import backgammon_env
 import network
 import tesauro
 
-layers = [198, 40, 4]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("m1")
+    parser.add_argument("m2")
+    args = parser.parse_args()
 
+    layers = [198, 40, 4]
 
-good = network.layered(*layers)
-good.load_state_dict(torch.load("model.2000.pt"))
+    good = network.layered(*layers)
+    good.load_state_dict(torch.load(args.m1))
 
-bad = network.layered(*layers)
-bad.load_state_dict(torch.load("model.1000.pt"))
+    bad = network.layered(*layers)
+    bad.load_state_dict(torch.load(args.m2))
 
+    bck = backgammon_env.Backgammon()
+    observer = tesauro.Tesauro198()
 
-bck = backgammon_env.Backgammon()
-observer = tesauro.Tesauro198()
+    import head_to_head
 
-import head_to_head
-
-results = head_to_head.trial(
-    (good, observer),
-    (bad, observer),
-    cb=lambda results: print(
-        results,
-        (-2 * results[0] + -1 * results[1] + results[2] + 2 * results[3])
-        / (sum(results)),
-    ),
-    games=1000,
-)
+    results = head_to_head.trial(
+        (good, observer),
+        (bad, observer),
+        cb=lambda results: print(
+            results,
+            (-2 * results[0] + -1 * results[1] + results[2] + 2 * results[3])
+            / (sum(results)),
+        ),
+        games=1000,
+    )
