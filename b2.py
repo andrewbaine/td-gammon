1405c3bbfed904b78aa887ba88a1ab18ca4797b3
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-03-26 18:59:12 -0400

    major bug fix with bear-off logic

--- b2.py
+++ b2.py
@@ -6,12 +6,9 @@ def _m2(board, result, d1, d2, best=0):
             if dominant_pip is None:
                 dominant_pip = i
             if not (
-                (board[dest1] < -1)  # occupied by opponent
-                if dest1 < 25
-                else (
-                    (dest1 == 25 and dominant_pip < 19)  # exact bearoff
-                    or (dest1 > 25 and dominant_pip != i)  # over-bearoff
-                )
+                (board[dest1] < -1)
+                if (dest1 < 25)
+                else ((dominant_pip < 19) if dest1 == 25 else (dominant_pip != i))
             ):
                 board[i] = pc1 - 1
                 if i == dominant_pip and board[i] == 0:
@@ -26,18 +23,20 @@ def _m2(board, result, d1, d2, best=0):
                 if d1 > best:
                     best = d1
                     result.add((best, ((25 - i, 25 - i - d1),)))
-                j = i
-                while j < 26:
+                for j in range(i, 26):
                     pc2 = board[j]
                     if pc2 > 0:
                         if dominant_pip is None:
                             dominant_pip = j
                         dest2 = j + d2
                         # if we can land on this dest1 or bearoff
-                        if (dest2 < 25 and board[dest2] > -2) or (
-                            dominant_pip is None
-                            or (
-                                dominant_pip > 18 and (dest2 == 25 or dominant_pip == j)
+                        if not (
+                            (board[dest2] < -1)
+                            if (dest2 < 25)
+                            else (
+                                (dominant_pip < 19)
+                                if dest2 == 25
+                                else (dominant_pip != j)
                             )
                         ):
                             best = d1 + d2
@@ -61,7 +60,6 @@ def _m2(board, result, d1, d2, best=0):
 
                         if j == 0:
                             break
-                    j += 1
                 if is_hit:
                     board[dest1] = -1
                 elif dest1 < 25:
@@ -85,12 +83,9 @@ def _dubs(board, result, d1, best=0):
             if dominant_pip is None:
                 dominant_pip = i
             if (
-                (board[dest1] < -1)  # occupied by opponent
+                (board[dest1] < -1)
                 if dest1 < 25
-                else (
-                    (dest1 == 25 and dominant_pip < 19)  # exact bearoff
-                    or (dest1 > 25 and dominant_pip != i)  # over-bearoff
-                )
+                else (dominant_pip < 19 if dest1 == 25 else dominant_pip != i)
             ):
                 continue
             board[i] = pc1 - 1
@@ -104,8 +99,7 @@ def _dubs(board, result, d1, best=0):
             if d1 > best:
                 best = d1
                 result.add((best, ((25 - i, 25 - i - d1),)))
-            j = i
-            while j < 26:
+            for j in range(i, 26):
                 pc2 = board[j]
                 if pc2 > 0:
                     dest2 = j + d1
@@ -114,14 +108,10 @@ def _dubs(board, result, d1, best=0):
                     if dominant_pip is None:
                         dominant_pip = j
                     if (
-                        (board[dest2] < -1)  # occupied by opponent
+                        (board[dest2] < -1)
                         if dest2 < 25
-                        else (
-                            (dest2 == 25 and dominant_pip < 19)  # exact bearoff
-                            or (dest2 > 25 and dominant_pip != j)  # over-bearoff
-                        )
+                        else (dominant_pip < 19) if dest2 == 25 else (dominant_pip != j)
                     ):
-                        j += 1
                         continue
                     board[j] = pc2 - 1
                     if j == dominant_pip and board[j] == 0:
@@ -136,8 +126,7 @@ def _dubs(board, result, d1, best=0):
                         result.add(
                             (best, ((25 - i, 25 - i - d1), (25 - j, 25 - j - d1)))
                         )
-                    k = j
-                    while k < 26:
+                    for k in range(j, 26):
                         pc3 = board[k]
                         if pc3 > 0:
                             dest3 = k + d1
@@ -149,13 +138,12 @@ def _dubs(board, result, d1, best=0):
                                 (board[dest3] < -1)  # occupied by opponent
                                 if dest3 < 25
                                 else (
-                                    (dest3 == 25 and dominant_pip < 19)  # exact bearoff
-                                    or (
-                                        dest3 > 25 and dominant_pip != k
-                                    )  # over-bearoff
+                                    (dominant_pip < 19)  # bearoff before bearin
+                                    if dest3 == 25
+                                    else dominant_pip
+                                    != k  # over-bearoff when not the dominant pip
                                 )
                             ):
-                                k += 1
                                 continue
                             board[k] = pc3 - 1
                             if k == dominant_pip and board[k] == 0:
@@ -177,19 +165,19 @@ def _dubs(board, result, d1, best=0):
                                         ),
                                     )
                                 )
-                            l = k
-                            while l < 26:
+                            for l in range(k, 26):
                                 pc4 = board[l]
                                 if pc4 > 0:
                                     if dominant_pip is None:
                                         dominant_pip = l
                                     dest4 = l + d1
-                                    # if we can land on this dest1 or bearoff
-                                    if (dest4 < 25 and board[dest4] > -2) or (
-                                        dominant_pip is None
-                                        or (
-                                            dominant_pip > 18
-                                            and (dest4 == 25 or dominant_pip == l)
+                                    if (
+                                        (board[dest4] > -2)
+                                        if dest4 < 25
+                                        else (
+                                            (dominant_pip > 18)
+                                            if dest4 == 25
+                                            else (dominant_pip == l)
                                         )
                                     ):
                                         best = 4 * d1
@@ -206,7 +194,6 @@ def _dubs(board, result, d1, best=0):
                                         )
                                     if l == 0:
                                         break
-                                l += 1
                             if is_hit3:
                                 board[dest3] = -1
                             elif dest3 < 25:
@@ -216,7 +203,6 @@ def _dubs(board, result, d1, best=0):
                                 dominant_pip = k
                             if k == 0:
                                 break
-                        k += 1
                     if is_hit2:
                         board[dest2] = -1
                     elif dest2 < 25:
@@ -226,7 +212,6 @@ def _dubs(board, result, d1, best=0):
                         dominant_pip = j
                     if j == 0:
                         break
-                j += 1
             if is_hit1:
                 board[dest1] = -1
             elif dest1 < 25:
--- human_v_model.py
+++ human_v_model.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 
 import backgammon
@@ -5,58 +7,92 @@ import backgammon_env
 import model
 import network
 import tesauro
+import policy
 
-layers = [198, 40, 4]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hidden", type=int, default=40)
+    parser.add_argument("--model", required=True)
+    parser.add_argument("--encoding", choices=["tesauro198"], required=True)
+    args = parser.parse_args()
 
-n = network.layered(*layers)
-n.load_state_dict(torch.load("model.700.pt"))
-n = network.with_utility(n)
+    bck = backgammon_env.Backgammon()
+    observe = None
 
+    match args.encoding:
+        case "tesauro198":
+            observe = tesauro.observe
+        case _:
+            assert False
+    assert observe is not None
 
-bck = backgammon_env.Backgammon()
-observer = tesauro.Tesauro198()
+    t = observe(bck.s0(player_1=True))
+    layers = [t.size()[0], args.hidden, 4]
 
-with torch.no_grad():
+    nn = network.layered(*layers)
+    nn.load_state_dict(torch.load(args.model))
+    nn = network.with_utility(nn)
+    policy = policy.Policy_1_ply(bck, observe, nn)
 
-    dice = backgammon.first_roll()
-    (d1, d2) = dice
-    human_first = d1 < d2
+    with torch.no_grad():
 
-    state = bck.s0()
-    while True:
-        (board, player_1) = state
-        done = bck.done(state)
-        if done is not None:
-            match done:
-                case -2:
-                    print(("human" if player_1 else "robot") + " won by 2 points")
-                case -1:
-                    print(("human" if player_1 else "robot") + " won 1 point")
-                case 1:
-                    print(("human" if not player_1 else "robot") + " won 1 point")
-                case 2:
-                    print(("human" if not player_1 else "robot") + " won by 2 points")
-                case _:
-                    raise Exception("unexpected")
-            break
-        if player_1 and human_first or (not player_1 and not human_first):
-            if human_first:
-                print(backgammon.to_str(board))
+        dice = backgammon.first_roll()
+        (d1, d2) = dice
+        human_first = d1 < d2
+
+        state = bck.s0()
+        while True:
+            (board, player_1) = state
+            b = [x for x in board]
+            if not human_first:
+                backgammon.invert(b)
+            print(backgammon.to_str(b, player_1_color=backgammon.Color.Dark))
+            print("Dice:", dice)
+            done = bck.done(state)
+            if done is not None:
+                match done:
+                    case -2:
+                        print(("human" if player_1 else "robot") + " won by 2 points")
+                    case -1:
+                        print(("human" if player_1 else "robot") + " won 1 point")
+                    case 1:
+                        print(("human" if not player_1 else "robot") + " won 1 point")
+                    case 2:
+                        print(
+                            ("human" if not player_1 else "robot") + " won by 2 points"
+                        )
+                    case _:
+                        raise Exception("unexpected")
+                break
+            humans_turn = player_1 and human_first or (not player_1 and not human_first)
+            if humans_turn:
+                move = None
+                moves = bck.available_moves(state, dice)
+                if moves:
+                    input("press [return] to see your options")
+                    for i, move in enumerate(moves):
+                        print(i, move)
+                    selection = None
+                    while True:
+                        try:
+                            selection = int(input("Select move: "))
+                            if -1 < selection and selection < len(moves):
+                                break
+                        except:
+                            pass
+                    assert selection is not None
+                    move = moves[selection]
+                    by_the_way = policy.choose_action((board, player_1, dice))
+                else:
+                    by_the_way = None
+                    input("sorry, you can't move, press [return]")
             else:
-                backgammon.invert(board)
-                print(backgammon.to_str(board))
-                backgammon.invert(board)
-            print("Roll:", dice)
-            move = None
-            moves = bck.available_moves(state, dice)
-            if moves:
-                for i, move in enumerate(moves):
-                    print(i, move)
-                i = int(input("Select move: "))
-                move = moves[i]
+                input("press enter to see computer move")
+                (board, player_1) = state
+                move = policy.choose_action((board, player_1, dice))
+                by_the_way = None
+            print("human" if humans_turn else "computer", "played", move)
+            if by_the_way:
+                print("by the way, computer liked", by_the_way)
             state = bck.next(state, move)
-        else:
-            move = model.best(bck, observer, state, dice, network)
-            print("Rolled", (d1, d2), "; played ", move)
-            state = bck.next(state, move)
-        dice = backgammon.roll()
+            dice = backgammon.roll()
