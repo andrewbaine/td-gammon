74e47cea6153634bbb63913884270e8ec16f57a9
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-04-04 16:06:16 -0400

    simplify move lookup

--- read_move_tensors.py
+++ read_move_tensors.py
@@ -4,6 +4,14 @@ from write_move_tensors import singles_dir, doubles_dir, ab_dir, noop_dir
 import os
 
 
+def transform():
+    rows = []
+    for i in range(26):
+        col = [0 if ((25 - j) == i) else -1 for j in range(26)]
+        rows.append(col)
+    return rows
+
+
 def read(path):
     return tuple(
         torch.load(os.path.join(path, x))
@@ -12,69 +20,60 @@ def read(path):
 
 
 def find_index(a, b):
-    if a == b:
-        assert False
     if a < b:
         (a, b) = (b, a)
-    assert a > b
+    assert a >= b
     match a:
-        case 2:  # 0
+        case 1:
+            assert b == 1
             return b - 1
-        case 3:  # 1, 2
+        case 2:  # 0
             return b
+        case 3:  # 1, 2
+            return b + 2
         case 4:  # 3, 4, 5
-            return 2 + b
+            return b + 5
         case 5:  # 6, 7, 8, 9
-            return 5 + b
+            return b + 9
         case 6:  # 10, 11, 12, 13, 14, 15
-            return 9 + b
+            return b + 14
         case _:
             assert False
 
 
 class MoveTensors:
     def __init__(self, dir, device="cpu"):
-        self.doubles = []
-        self.singles = []
         self.ab = []
 
         with torch.device(device):
-            self.noop = read(noop_dir(dir))
+            noop = read(noop_dir(dir))
+            singles = [read(singles_dir(dir, d1)) for d1 in range(1, 7)]
+
             for d1 in range(1, 7):
-                self.singles.append(read(singles_dir(dir, d1)))
-                dubsies = [
-                    read(doubles_dir(dir, d1, name)) for name in ["4", "3", "2", "1"]
-                ]
-                dubsies.append(self.noop)
-                self.doubles.append(dubsies)
-                for d2 in range(1, d1):
-                    self.ab.append(read(ab_dir(dir, d1, d2)))
+                for d2 in range(1, d1 + 1):
+                    if d1 == d2:
+                        dubsies = [
+                            read(doubles_dir(dir, d1, name))
+                            for name in ["4", "3", "2", "1"]
+                        ]
+                        dubsies.append(noop)
+                        self.ab.append(dubsies)
+                    else:
+                        xs = [read(ab_dir(dir, d1, d2))]
+                        xs.append(singles[d1 - 1])
+                        xs.append(singles[d2 - 1])
+                        xs.append(noop)
+                        self.ab.append(xs)
 
     def compute_moves(self, state):
         (board, player_1, (d1, d2)) = state
         assert player_1
-        if d1 == d2:
-            dubsies = self.doubles[d1 - 1]
-            for moves, lower, upper, vector in dubsies:
-                indices = torch.all(lower <= board, dim=1) & torch.all(
-                    upper > board, dim=1
-                )
-                ms = moves[indices]
-                if ms.size()[0] > 0:
-                    return ms
-        else:
-            i = find_index(d1, d2)
-            big, small = (d1, d2) if d1 > d2 else (d2, d1)
-            for moves, lower, upper, vector in [
-                self.ab[i],
-                self.singles[big - 1],
-                self.singles[small - 1],
-                self.noop,
-            ]:
-                indices = torch.all(lower <= board, dim=1) & torch.all(
-                    upper > board, dim=1
-                )
-                ms = moves[indices]
-                if ms.size()[0] > 0:
-                    return ms
+        tensors = None
+        i = find_index(d1, d2)
+        tensors = self.ab[i]
+        for moves, lower, upper, vector in tensors:
+            indices = torch.all(lower <= board, dim=1) & torch.all(upper > board, dim=1)
+            ms = moves[indices]
+            if ms.size()[0] > 0:
+                return ms
         assert False
