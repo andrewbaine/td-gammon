d06844d8d24181d2b8f9966fdc9b5348ea71f270
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-04-11 12:27:10 -0400

    scripts to build tensors and train

---
+++ scripts/build_move_tensors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+D=$(date +%s)
+
+mkdir -p var/move_tensors
+
+docker run \
+       --mount type=bind,src=$(PWD)/var/move_tensors,target=/var/move_tensors \
+       td-gammon move-tensors --prefix /var/move_tensors/tensors-${D}
+
+pushd var/move_tensors
+pwd
+rm -f current
+ln -sf tensors-${D} current
+popd
---
+++ scripts/train.sh
@@ -0,0 +1,50 @@
+set -e
+set -x
+
+while getopts ":g:m:o:" opt; do
+    case $opt in
+        g)
+            GAMES="${OPTARG}"
+            ;;
+        m)
+            MODEL="${OPTARG}"
+            ;;
+        o)
+            OUT="${OPTARG}"
+            ;;
+        *)
+            exit 1
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+if [ -z "$GAMES" ]
+then
+    exit 1
+fi
+if [ -z "$MODEL" ]
+then
+    exit 1
+fi
+if [ -z "$OUT" ]
+then
+    exit 1
+fi
+
+if docker run --gpus all hello-world; then
+    GPU_ARGS="--gpus all";
+else
+    GPU_ARGS="";
+fi
+
+docker run --rm \
+       $GPU_ARGS \
+       --mount type=bind,src=$(PWD)/var/move_tensors,target=/var/move_tensors \
+       --mount type=bind,src=$(PWD)/var/models,target=/var/models \
+       td-gammon train \
+       --move-tensors /var/move_tensors/current \
+       --save-dir /var/models/${MODEL} \
+       --out $OUT \
+       --iterations $GAMES
+
