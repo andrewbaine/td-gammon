54e7f3fc74f174a95dd4b6752ceb5ecaf18d6984
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-04-11 21:55:17 -0400

    this encoding works but theres some duplicated info

--- src/baine_encoding.py
+++ src/baine_encoding.py
@@ -1,11 +1,15 @@
 import torch
 
-from torch import matmul, maximum, minimum
+from torch import matmul, maximum, minimum, float
+
+
+def tensor(data):
+    return torch.tensor(data, dtype=float)
 
 
 def barrier_matrix(b):
     m = []
-    for n in range(2, 8):
+    for n in range(1, 8):
         x = []
         m.append(x)
         for i in range(26):
@@ -17,45 +21,24 @@ def barrier_matrix(b):
                     if (i == 0 or i == 25 or j == 0 or j == 25)
                     else 1 if (-1 < ((i - j) if b else (j - i)) < n) else 0
                 )
-    return torch.tensor(m)
+    return tensor(m)
 
 
 def additions():
     m = []
-    for n in range(2, 8):
+    for n in range(1, 8):
         m.append([(n - 1) for _ in range(26)])
-    return torch.tensor(m)
-
-
-def f(board):
-    m = barrier_matrix(True)
-    y = torch.matmul(
-        torch.where(board > 1, torch.ones_like(board), torch.zeros_like(board)), m
-    )
-    z = y - additions()
-    p = torch.maximum(z, torch.zeros_like(z))
-    x = torch.tensor([[j + 2 if i == j else 0 for j in range(6)] for i in range(6)])
-    q = torch.matmul(x, p).max(dim=0).values
-    m = barrier_matrix(False)
-    y = torch.matmul(
-        torch.where(board < -1, torch.ones_like(board), torch.zeros_like(board)), m
-    )
-    z = y - additions()
-    p = torch.maximum(z, torch.zeros_like(z))
-    x = torch.tensor([[-j - 2 if i == j else 0 for j in range(6)] for i in range(6)])
-    q2 = torch.matmul(x, p).min(dim=0).values
-    return q + q2
-
-
-#    return q + q2
+    return tensor(m)
 
 
 class Encoder:
     def __init__(self):
+        self.player_1_barrier_matrix = barrier_matrix(True)
+        self.player_2_barrier_matrix = barrier_matrix(False)
+        self.additions = additions()
         matrix = []
         floor = []
         ceil = []
-
         cap = []
         addition = []
         floor_2 = []
@@ -80,17 +63,14 @@ class Encoder:
                         ceil.append(k + 2)
                         floor.append(k + 1)
                         ceil.append(k + 2)
-
                         floor_2.append(0)
                         ceil_2.append(1)
                         floor_2.append(0)
                         ceil_2.append(1)
-
                         addition.append(0)
                         addition.append(0)
                         cap.append(1)  # unary encoding for x = 1, 2, or 3
                         cap.append(1)  # unary encoding for x = -1, -2, or -3
-
                 row.append(1 if i == j else 0)
                 row.append(-1 if i == j else 0)
                 if i == 0:
@@ -98,17 +78,14 @@ class Encoder:
                     ceil.append(16)
                     floor.append(0)
                     ceil.append(16)
-
                     floor_2.append(0)
                     ceil_2.append(16)
                     floor_2.append(0)
                     ceil_2.append(16)
-
                     addition.append(-3)
                     addition.append(-3)
                     cap.append(15)
                     cap.append(15)
-
             row.append(1 if i == 25 else 0)  # player 1 bar
             if i == 25:
                 floor.append(0)
@@ -117,14 +94,12 @@ class Encoder:
                 ceil_2.append(15)
                 addition.append(0)
                 cap.append(15)
-
             for _ in range(4):
                 row.append(0)
             assert len(row) == 198
         for x in [floor, ceil, floor_2, ceil_2, addition]:
             for _ in range(4):
                 x.append(0)
-
         for i in range(4):
             cap.append(15)
         self.matrix = torch.tensor(matrix, dtype=torch.float)
@@ -134,12 +109,10 @@ class Encoder:
         self.ceil_2 = torch.tensor(ceil_2, dtype=torch.float)
         self.addition = torch.tensor(addition, dtype=torch.float)
         self.cap = torch.tensor(cap, dtype=torch.float)
-
         count_white = [[1 if i == 194 else 0 for i in range(198)] for _ in range(26)]
         count_black = [[-1 if i == 195 else 0 for i in range(198)] for _ in range(26)]
         self.count_white_pieces = torch.tensor(count_white, dtype=torch.float)
         self.count_black_pieces = torch.tensor(count_black, dtype=torch.float)
-
         self.zero_board = torch.tensor([0 for _ in range(26)], dtype=torch.float)
         self.white_turn = torch.tensor(
             [(1 if i == 196 else 0) for i in range(198)],
@@ -163,7 +136,6 @@ class Encoder:
             [0 for _ in range(198)],
             dtype=torch.float,
         )
-
         scale = []
         for i in range(198):
             row = []
@@ -193,16 +165,25 @@ class Encoder:
             scale,
             dtype=torch.float,
         )
+        self.square = tensor(
+            [[j + 1 if i == j else 0 for j in range(7)] for i in range(7)]
+        )
+        self.square_neg = tensor(
+            [[-j - 1 if i == j else 0 for j in range(7)] for i in range(7)]
+        )
+        self.zeroes = tensor([0 for _ in range(26)])
+        self.ones = tensor([1 for _ in range(26)])
+        self.many_zeroes = torch.matmul(self.zeroes, self.player_1_barrier_matrix)
 
     def encode(self, board, player_1):
-        y = matmul(board, self.matrix) + self.addition
-
-        condition = torch.logical_and(self.floor <= y, y < self.ceil)
-        y = torch.where(condition, y, self.zero_tensor)
-        y = torch.minimum(y, self.cap)
-
-        y = y + matmul(maximum(board, self.zero_board), self.count_white_pieces)
-        y = y + matmul(minimum(board, self.zero_board), self.count_black_pieces)
-        y = y + (self.white_turn if player_1 else self.black_turn)
-        y = matmul(y, self.scale)
-        return y
+        m = self.player_1_barrier_matrix
+        y = torch.matmul(torch.where(board > 1, self.ones, self.zeroes), m)
+        z = y - self.additions
+        p = torch.maximum(z, self.many_zeroes)
+        q = torch.matmul(self.square, p).max(dim=0).values
+        m2 = self.player_2_barrier_matrix
+        y2 = torch.matmul(torch.where(board < -1, self.ones, self.zeroes), m2)
+        z2 = y2 - self.additions
+        p2 = torch.maximum(z2, self.many_zeroes)
+        q2 = torch.matmul(self.square_neg, p2).min(dim=0).values
+        return q + q2
