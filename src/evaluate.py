b766be288ce12c649cdd32ed17348765b4d5902b
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-04-12 18:06:56 -0400

    fixbug

--- src/evaluate.py
+++ src/evaluate.py
@@ -38,7 +38,7 @@ def main(args):
 
     if torch.cuda.is_available() or args.force_cuda:
         device = torch.device("cuda")
-        nn = nn.to_(device)
+        nn = nn.to(device)
         move_tensors.to_(device)
         encoder.to_(device)
         utility = utility.to(device)
