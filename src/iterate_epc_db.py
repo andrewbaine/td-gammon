236597bc8629b9584052b115fc6cc01a31c2f0f6
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-04-18 21:23:06 -0400

    count or lookup

--- src/iterate_epc_db.py
+++ src/iterate_epc_db.py
@@ -3,21 +3,35 @@ import struct
 import plyvel
 
 
-def main(db_path, board):
-
-    with plyvel.DB(db_path, create_if_missing=True) as db:
+def lookup(db_path, board):
+    with plyvel.DB(db_path, create_if_missing=False) as db:
         k = bytes(board)
         v = db.get(k)
         (v,) = struct.unpack("f", v)
         print(v, v * 49 / 6)
 
 
+def count(db_path):
+    with plyvel.DB(db_path, create_if_missing=False) as db:
+        with db.iterator() as it:
+            count = 0
+            for _ in it:
+                count += 1
+            print(count)
+
+
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db", type=str)
-    parser.add_argument("board", default="")
+    parser.add_argument("--db", type=str, required=True)
+    parser.add_argument("--board", default="")
+    parser.add_argument("action", choices=["count", "lookup"])
+
     args = parser.parse_args()
-    board = [int(x) for x in args.board.split(",")] if args.board else []
-    main(args.db, board)
+    match args.action:
+        case "count":
+            count(args.db)
+        case "lookup":
+            board = [int(x) for x in args.board.split(",")] if args.board else []
+            lookup(args.db, board)
